95051530eddd41cf93389281de29d11d9d1d6cef
Bishrul Haq <bishhaq@gmail.com>
2024-01-06 10:06:46 +0530

    add category added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,9 +31,9 @@
                 </div>
                 <div class="col-xl-3 col-md-6">
                     <div class="card bg-success text-white mb-4">
-                        <div class="card-body">Available Products</div>
+                        <div class="card-body">Categories</div>
                         <div class="card-footer d-flex align-items-center justify-content-between">
-                            <a class="small text-white stretched-link" href="{{ route('available.products') }}">View
+                            <a class="small text-white stretched-link" href="{{ route('add.category') }}">View
                                 Details</a>
                             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                         </div>
